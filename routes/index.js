01db92866ca4e9047c5f2e1a9c8fbf4a13af591e
alejandrocsdev@gmail.com <alejandrocsdev@gmail.com>
2026-04-25 19:05:57 +0800

    remove aws s3 related implementation

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,10 @@ const authRoutes = require('./auth.routes');
 const usersRoutes = require('./users.routes');
 const comfyuiRoutes = require('./comfyui.routes');
 const runpodRoutes = require('./runpod.routes');
-const s3Routes = require('./s3.routes');
 
 router.use('/auth', authRoutes);
 router.use('/users', usersRoutes);
 router.use('/comfyui', authenticate, comfyuiRoutes);
 router.use('/runpod', authenticate, runpodRoutes);
-router.use('/s3', authenticate, s3Routes);
 
 module.exports = router;
